ff0a4eb899cd62b6c743e9de430ba69eb449d04b
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-06-18 15:10:19 +0500

    Game Box Ready, Little but Animations Added

--- Index.js
+++ Index.js
@@ -2,7 +2,8 @@ let GetPlayerNames=()=>
 {
     let P1=document.querySelector('.P1').value;
     let P2=document.querySelector('.P2').value;
-    
-    console.log(P1);
-    console.log(P2);
+
+    let Form=document.querySelector('.Form').style.display="none";
+    let GameBox=document.querySelector('.Game-Box').style.display="grid";
+    document.querySelector('.Game-Box').classList.add('Slide');
 }
