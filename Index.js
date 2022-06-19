4e5dba3f6ae322864d8cf3a5af5e6e3c2db1070a
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-06-19 14:48:04 +0500

    Display Results if User Wins

--- Index.js
+++ Index.js
@@ -24,11 +24,107 @@ let GetPlayerNames=()=>
         document.querySelector('.Turns p').innerHTML=`${P2} Turn`;
     }
 }
-let Decision=(Sign)=>
-{
-
-};
 let X=document.querySelectorAll('.Box')
+
+let Decision=(Sign , Player)=>
+{
+    let Num=0;
+    let  Count1=0;
+    let  Count2=0;
+    let  Count3=0;
+    let  Count4=0;
+    let Arr=new Array(3);
+    for(let k=0 ; k<Arr.length ; k++)
+    {
+        Arr[k]=new Array(3);
+    }
+
+    for(let i=1 ; i<4 ; i++)
+    {
+        for(let j=1 ; j<4 ; j++)
+        {
+            Arr[i-1][j-1]=document.getElementById(X[Num].id).innerHTML;
+            Num++;
+        }
+    }
+    
+    for(let i=1 ; i<4 ; i++)
+    {
+        for(let j=1 ; j<4 ; j++)
+        {
+            if(Arr[i-1][j-1]==Sign)
+            {
+                Count1++;
+                if(Count1==3)
+                {
+                    document.querySelector('.Heading').innerHTML=`Hurray 🎉🥂 ${Player} Won`;
+                    document.querySelector('.Heading').style.display="grid";
+                    X.forEach(Value=>
+                    {
+                        Value.classList.add('Make-None');
+                    });
+                    Count1=0;
+                    return;
+                }
+            }
+            if(Arr[j-1][i-1]==Sign)
+            {
+                Count2++;
+                if(Count2==3)
+                {
+                    document.querySelector('.Heading').innerHTML=`Hurray 🎉🥂 ${Player} Won`;
+                    document.querySelector('.Heading').style.display="grid";
+                    X.forEach(Value=>
+                    {
+                        Value.classList.add('Make-None');
+                    });
+                    Count2=0;
+                    return;
+                }
+            }
+        }
+        Count1=0;
+        Count2=0;
+    }
+    for(let l=0 ; l<3 ; l++)
+    {
+        if(Arr[l][l]==Sign)
+        {
+            Count3++;
+            if(Count3==3)
+            {
+                document.querySelector('.Heading').innerHTML=`Hurray 🎉🥂 ${Player} Won`;
+                document.querySelector('.Heading').style.display="grid";
+                X.forEach(Value=>
+                {
+                    Value.classList.add('Make-None');
+                });
+                Count3=0;
+                return;
+            }
+        }
+    }
+    let Z=0 , Y=2;
+    while(Z<=3 && Y>=0)
+    {
+        if(Arr[Z][Y]==Sign)
+        {
+            Count4++;
+            if(Count4==3)
+            {
+                document.querySelector('.Heading').innerHTML=`Hurray 🎉🥂 ${Player} Won`;
+                document.querySelector('.Heading').style.display="grid";
+                X.forEach(Value=>
+                {
+                    Value.classList.add('Make-None');
+                });
+                Count4=0;
+                return;
+            }
+        }
+        Z++;Y--;
+    }
+};
 
 X.forEach(element => {
     element.addEventListener('click' , ()=>
@@ -37,8 +133,9 @@ X.forEach(element => {
         {
             while(document.getElementById(element.id).innerHTML=='')
             {
-                document.querySelector('.Turns p').innerHTML=`${P2} Turn`;
                 document.getElementById(element.id).innerHTML='X';
+                Decision('X' , P1);
+                document.querySelector('.Turns p').innerHTML=`${P2} Turn`;
                 PLYR=1;
             }
         }
@@ -46,8 +143,9 @@ X.forEach(element => {
         {
             while(document.getElementById(element.id).innerHTML=='')
             {
-                document.querySelector('.Turns p').innerHTML=`${P1} Turn`;
                 document.getElementById(element.id).innerHTML='O';
+                Decision('O' , P2);
+                document.querySelector('.Turns p').innerHTML=`${P1} Turn`;
                 PLYR=0;
             }
         }
@@ -74,5 +172,7 @@ document.querySelector('.fa-rotate-right').addEventListener('click' , Clear=()=>
    X.forEach(Value=>
     {
         document.getElementById(Value.id).innerHTML='';
+        document.querySelector('.Heading').style.display="none";
+        Value.classList.remove('Make-None');
     });
 });
